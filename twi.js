e6a41cea51c81c2c4f6f3e374265014dbc56defb
Todd <twitzel@gmail.com>
2013-10-08 17:37:04 -0600

    added timed writes to serial port in cs4 playing with mongo in cs-4

--- twi.js
+++ twi.js
@@ -37,7 +37,7 @@ exports.socketDataOut  = function(data)
    if (global.collectionLog){
     var serialData1=serialData;
        serialData1.Time = serialData.Time;
-       serialData1.UnitID = 5;
+     //  serialData1.UnitID = 5;
        serialData1.irTemp0 = 0;
        serialData1.irTemp1 = 1;
        serialData1.oneWire0 = 2;
