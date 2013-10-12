4a4f9082970ddc3467919adf839c371746b5f56a
twitzel <twitzel@gmail.com>
2013-10-11 18:38:51 -0600

    realtime current data graph

--- cs4.js
+++ cs4.js
@@ -167,7 +167,8 @@ exports.socketDataOut = function (data) {
                {
                     dataToSend[count] = item[0].OutData[count][0].Dout;
                     delay[count] = item[0].OutData[count][0].Delay;
-                    setTimeout(function() {sendOutput(dataToSend[count]);}, delay[count]);
+                    setTimeout(function() {
+                        console.log(dataToSend[count]);}, delay[count]);
                     console.log(item[0].OutData[count][0].Dout + "  Delay "+item[0].OutData[count][0].Delay);
                 }
 
