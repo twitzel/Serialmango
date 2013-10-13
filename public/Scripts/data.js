fb2efc08c907d759b830a5a3d49e47989a25ae8f
Todd <twitzel@gmail.com>
2013-10-12 21:50:28 -0600

    working test.js with samples from the last minute

--- public/Scripts/data.js
+++ public/Scripts/data.js
@@ -17,7 +17,6 @@ function graphskeleton(prop)
     var loffset = 50;
     var toffset=20;
     graph[prop] = {};
-    graph[prop]="graph";
     graph[prop].low = low;
     graph[prop].high = high;
     graph[prop].id=document.getElementById(prop);
@@ -32,7 +31,7 @@ function graphskeleton(prop)
     graph[prop].context.moveTo(loffset,(graph[prop].id.height-(dp[0][prop]-low)*graph[prop].degperpixel));
     graph[prop].loffset = loffset;
     graph[prop].toffset = toffset;
-    graph[prop].created = true;
+
     for (var i = 0; i<dp.length;++i)
     {
         if (dp[i][prop]){
