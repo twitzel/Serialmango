8b2a3fe6cc6822f154ef10ce2bd6f34a2d8852d6
twitzel <twitzel@gmail.com>
2013-10-12 18:38:03 -0600

    working graphclick

--- public/Scripts/data.js
+++ public/Scripts/data.js
@@ -1,7 +1,7 @@
 
 var wsUri = "ws://localhost:8080";
 var output;
-var graph =new Object;
+var graph ={};
 
 
 //  c.font = "14px sans-serif";
@@ -15,23 +15,26 @@ function graphskeleton(prop)
         if (dp[i][prop] < low){low = dp[i][prop];}
     }
     var loffset = 50;
-    graph[prop] = new Object;
+    var toffset=20;
+    graph[prop] = {};
     graph[prop].low = low;
     graph[prop].high = high;
     graph[prop].id=document.getElementById(prop);
     graph[prop].id_rt=document.getElementById(prop+"_rt"); // realtime graph
     graph[prop].context = graph[prop].id.getContext("2d");
     graph[prop].context_rt = graph[prop].id_rt.getContext("2d");
-    graph[prop].degperpixel = (graph[prop].id.height)/(high-low);
+    graph[prop].degperpixel = (graph[prop].id.height-toffset)/(high-low);
     graph[prop].context.lineWidth = 1;
     graph[prop].context.strokeStyle = "rgb(0,0,0)";
+    graph[prop].context.clearRect (0 , 0 , graph[prop].id.width , graph[prop].id.height );
     graph[prop].context.beginPath();
     graph[prop].context.moveTo(loffset,(graph[prop].id.height-(dp[0][prop]-low)*graph[prop].degperpixel));
-
+    graph[prop].loffset = loffset;
+    graph[prop].toffset = toffset;
     for (var i = 0; i<dp.length;++i)
     {
         if (dp[i][prop]){
-            graph[prop].context.lineTo(i+loffset,(graph[prop].id.height-(dp[i][prop]-low)*graph[prop].degperpixel));
+            graph[prop].context.lineTo(i+graph[prop].loffset,(graph[prop].id.height-(dp[i][prop]-graph[prop].low)*graph[prop].degperpixel));
         } else
         { //no sensor data
             // graph[prop].context.lineTo(i+loffset,graph[prop].id.height);
@@ -40,21 +43,51 @@ function graphskeleton(prop)
     graph[prop].context.stroke();
     graph[prop].context.fillStyle = "blue";
     graph[prop].context.font = "16px Arial";
-    graph[prop].context.fillText(high,2,13);
+    graph[prop].context.fillText(high,2,13+toffset);
     graph[prop].context.fillText(low,2,graph[prop].id.height-1);
     graph[prop].data = [];
 }
+function graphclick(){
+    var prop = this.id;
+    graphskeleton(prop);
+    graph[prop].context.beginPath();
+    graph[prop].context.moveTo(graph[prop].loffset,event.offsetY);
+    graph[prop].context.lineTo(this.width,event.offsetY);
+    graph[prop].context.stroke();
 
+    var lineval = ((((graph[prop].id.height-event.offsetY)/graph[prop].degperpixel)+graph[prop].low)*100);
+    lineval = Math.round(lineval)/100;
+    //*graph[prop].degperpixel
+    graph[prop].context.fillStyle = "red";
+    graph[prop].context.fillText(lineval,2,event.offsetY+6);
+    if (event.offsetX > graph[prop].loffset){
+
+
+        if (dp[event.offsetX-graph[prop].loffset][prop]) {
+            graph[prop].context.strokeStyle = "green";
+            graph[prop].context.beginPath();
+            graph[prop].context.arc(event.offsetX,(graph[prop].id.height-(dp[event.offsetX-graph[prop].loffset][prop]-graph[prop].low)*graph[prop].degperpixel),
+            5,0,2*Math.PI);
+            graph[prop].context.stroke();
+            graph[prop].context.fillStyle = "green";
+            graph[prop].context.fillText(dp[event.offsetX-graph[prop].loffset][prop],event.offsetX,13);
+        }
+
+    }
+
+}
 
 function nameclick(){
    // alert(this);
     tempwhatwasclicked=this;
 
-    this.innerHTML=("<input type='text' onkeydown='if (event.keyCode == 13) blurtest()' onBlur='blurtest()' autofocus='autofocus' value='"+this.dataset.name+"'> "+this.dataset.sensor );
+    this.innerHTML=("<input type='text' onkeydown='if (event.keyCode == 13) blurtest()'" +
+        " onBlur='blurtest()' autofocus='autofocus' value='"+this.dataset.name+"'> "+
+        this.dataset.sensor );
 
-}function blurtest(par){
+}function blurtest(){
     tempwhatwasclicked.innerHTML= tempwhatwasclicked.firstChild.value;
-    var sendobj = new Object;
+    var sendobj = {};
     sendobj.packettype="Sensor name update";
     sendobj.sensor =tempwhatwasclicked.dataset.sensor;
     sendobj.name = tempwhatwasclicked.innerHTML;
