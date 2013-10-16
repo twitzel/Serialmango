9d36df6ea715e2a8fbe6fbd844c06d339cd49ff8
stevewitz <switzel@wizcomputing.com>
2013-10-16 13:32:54 -0700

    removed log ports - doesn't work on PI.  works on pc

--- cs4.js
+++ cs4.js
@@ -69,7 +69,8 @@ sendOutput = function (dataToSend)
 exports.setup = function()
 {
     //MongoClient.connect("mongodb://localhost:27017/WizDb", function(err, db)
-    MongoClient.connect("mongodb://192.168.2.10:27017/WizDb", function(err, db)
+   // MongoClient.connect("mongodb://192.168.2.10:27017/WizDb", function(err, db)
+    MongoClient.connect("mongodb://" + address[0] + ":27017/WizDb", function(err, db)
     {
         if (err)
         {
