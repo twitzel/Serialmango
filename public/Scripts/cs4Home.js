ebd8316ca887b31ba612be65489ff328a7129c3e
stevewitz <switzel@wizcomputing.com>
2013-10-22 11:32:34 -0700

    many new pages added

--- public/Scripts/cs4Home.js
+++ public/Scripts/cs4Home.js
@@ -1,19 +1,37 @@
 /**
  * Created by Steve on 10/21/13.
  */
-var counter = 0;
-var lastCueTime;
-var delay;
-var wsUri = "ws://" + window.location.hostname + ":8080";
-
-
 
 window.onload = init;
 function init(){
+    wsUri = "ws://" + window.location.hostname + ":8080";
     output = document.getElementById("websocketlog");
-    testWebSocket();}
 
-function testWebSocket(){
+    canvas = document.getElementById('myCanvas');
+    context = canvas.getContext('2d');
+    canvasWidth = canvas.width;
+    canvasStart = canvasWidth*.8;
+    canvasHeight = canvas.height;
+    pixelData = new Array(); //time: ,line: , data,
+    numberLoops = 0;
+    ticIncrement = 10; // this gives a time increment of 1 ms per pixel //
+    lineStart = 180; // bottom of tic line
+
+     //setup origional time marks and time in the array
+    for(var i = 0; i  <=canvasStart; i+=ticIncrement){
+        if(i%50 == 0){
+            pixelData[i] = {line : 40};
+        }
+        else{
+            pixelData[i] = {line : 20};
+        }
+    }
+    testWebSocket();
+    setInterval(function(){movedata()},30);
+}
+
+function testWebSocket()
+{
     websocket = new WebSocket(wsUri);
     websocket.onopen = function(evt) { onOpen(evt) };
     websocket.onclose = function(evt) { onClose(evt) };
@@ -23,11 +41,11 @@ function testWebSocket(){
 
 function onOpen(evt) {
     writeToScreen("CONNECTED");
+    writeToScreen('#{myuri}');
 }
 
 function onClose(evt) {
     writeToScreen("DISCONNECTED");
-    setTimeout(init(),500); // if disconnected - wait 500 ms then try to connect again
 }
 
 function onMessage(evt)    {
@@ -43,9 +61,47 @@ function doSend(message) {
 }
 
 function writeToScreen(message) {
-
     // get time of incoming cue
     lastCueTime = new Date();
- //   output.innerHTML = message + "<BR>" + output.innerHTML;
+    output.innerHTML = message + "<BR>" + output.innerHTML;
+    pixelData[canvasStart] = {data : message};
+
     //output.value = message+"<BR>"+output.value;
+}
+
+function movedata(){
+    numberLoops++;
+    pixelData.shift();  //movers everything over 1 px
+
+    context.clearRect(0,0,canvas.width,canvas.height);
+    if(numberLoops%100 == 0){
+        pixelData[canvasStart] = {line : 40, time : new Date().toLocaleTimeString().substring(0,8)};
+    }
+    else if(numberLoops % ticIncrement == 0){
+        pixelData[canvasStart] = {line : 20};
+    }
+    context.beginPath();
+
+    for(var i = 0; i< pixelData.length; i++ ){
+        if(pixelData[i]){
+            if(pixelData[i].line){
+                context.moveTo(i,lineStart);
+                context.lineTo(i,lineStart -pixelData[i].line);
+            }
+            if(pixelData[i].time) {
+                context.fillText(pixelData[i].time,i-21 ,192)
+
+            }
+        }
+    }
+    context.stroke();
+    //add starting line
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.moveTo(canvasStart, 0);
+    context.lineTo(canvasStart, canvasHeight);
+    context.stroke();
+    context.strokeStyle = 'black';
+
+    context.stroke();
 }
