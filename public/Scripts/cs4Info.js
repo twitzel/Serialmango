fd1f2aeac6d7ab146a8dcf59dfb59501d1441938
stevewitz <switzel@wizcomputing.com>
2013-11-05 13:29:08 -0800

    added info

--- public/Scripts/cs4Info.js
+++ public/Scripts/cs4Info.js
@@ -7,13 +7,15 @@ function init()
     outputStartup = document.getElementById("startup");
     outputCountcue = document.getElementById("countcue");
     outputCountlog = document.getElementById("countlog");
+    outputTitles = document.getElementById("titles");
+
+    outputTitles.innerHTML = "System:" ;
 
 
-    outputStartup.innerHTML = "System last started on:" ;
-
-    for(var i = 0; i < startup.length; i++)
+    outputStartup.innerHTML =  "1.  " + new Date(startup[0].Time).toString().substr(0,25); // this to eliminate blank line at top
+    for(var i = 1; i < startup.length; i++)
     {
-        outputStartup.innerHTML = outputStartup.innerHTML +"<BR>" + startup[i].Time;
+        outputStartup.innerHTML = outputStartup.innerHTML +"<BR>" + (parseInt(i)+1) + ".  " +new Date(startup[i].Time).toString().substr(0,25) ;
     }
 
     outputStartup.innerHTML =   outputStartup.innerHTML + "<BR>" +"Number of Startups:" + "<BR>" + countStartup;
