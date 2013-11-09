35643fe2327c52d359de0b87f9dd8432ea044fbe
stevewitz <switzel@wizcomputing.com>
2013-11-09 14:28:58 -0800

    new Menu

--- public/Scripts/cs4Info.js
+++ public/Scripts/cs4Info.js
@@ -11,32 +11,41 @@ function init()
     outputTitles = document.getElementById("titles");
     outputUpTime = document.getElementById("uptime");
 
-    var uptime1;
-    var ti = new Date(startup[0].Time);
-    curTime = new Date(curTime);
-    uptime1 = curTime - ti; var x;
-    uptime1 = uptime1 / 1000;
-    seconds = Math.round(uptime1 % 60);
-    uptime1 /= 60;
-    minutes = Math.round(uptime1 % 60);
-    uptime1 /= 60;
-    hours = Math.round(uptime1 % 24);
-    uptime1 /= 24;
-    days = Math.round(uptime1);
-
-
-    outputUpTime.innerHTML = "System Up Time:" + "<br>" + uptime1 ;
+  UpTime();
 
     outputStartup.innerHTML =  "1.  " + new Date(startup[0].Time).toString().substr(0,25); // this to eliminate blank line at top
-    outputStartup.innerHTML = "Last 25 Restarts:" + "<br>" + outputStartup.innerHTML;
+    outputStartup.innerHTML = "Last 25 Restarts:".bold() + "<br>" + outputStartup.innerHTML;
     for(var i = 1; i < startup.length; i++)
     {
         outputStartup.innerHTML = outputStartup.innerHTML +"<BR>" + (parseInt(i)+1) + ".  " +new Date(startup[i].Time).toString().substr(0,25) ;
     }
 
-    outputCountstartup.innerHTML =  "Number of Startups:" + "<BR>" + countStartup;
+    outputCountstartup.innerHTML =  "Number of Restarts:".bold() + "<BR>" + countStartup;
 
-    outputCountcue.innerHTML = "Number of Cues:" + "<BR>" + countCue;
-   outputCountlog.innerHTML = "Number of Entries in Log File:" + "<BR>" + countLog;
+    outputCountcue.innerHTML = "Number of Cues:".bold() + "<BR>" + countCue;
+   outputCountlog.innerHTML = "Number of Entries in Log File:".bold() + "<BR>" + countLog;
 
+}
+
+function UpTime()
+{
+    var uptime;
+    var ti = new Date(startup[0].Time);
+    curTime = new Date();
+    uptime = curTime - ti;
+    uptime = uptime / 1000;
+    seconds = Math.round(uptime % 60);
+    if(seconds <10){seconds = "0" + seconds}
+    uptime /= 60;
+    minutes = Math.round(uptime % 60);
+    if(minutes <10){minutes = "0" + minutes}
+    uptime /= 60;
+    hours = Math.round(uptime % 24);
+    if(hours <10){hours = "0" + hours}
+    uptime /= 24;
+    days = Math.round(uptime);
+    uptime = days + " Days " + hours + " Hrs " + minutes + " Min " + seconds + " Sec";
+
+    outputUpTime.innerHTML = "System Up Time:".bold() + "<br>" + uptime ;
+    setTimeout(function(){UpTime();}, ( 1000));
 }
