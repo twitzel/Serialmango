5af46cf885243eb613f39848c1458c1a2738e2ca
stevewitz <switzel@wizcomputing.com>
2013-12-20 13:47:16 -0800

    autorun file added

--- autorun.sh
+++ autorun.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 #script file to remove mongod.lock if it exists
-
-#script file - notes
-#         /home/pi/Serialmango/autorun.sh
-#MUST : chmod 755 sutorun.sh
-#TO RUN Manually:  /home/pi/Serialmango/autorun.sh
-
-
 if [ -f  /data/db/mongod.lock ];
 then
-        rm  /data/db/mongod.lock
-        echo  "File deleted"
+	rm  /data/db/mongod.lock
+	echo  "File deleted"
 else
-        echo  "File does not exist"
+	echo  "File does not exist"
 fi
+cd Serialmango
 mongod &
 node app cs4
