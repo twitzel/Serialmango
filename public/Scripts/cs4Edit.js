8cc1dd99e2ab001f1a991c8c3ea09f5866934c61
stevewitz <switzel@wizcomputing.com>
2014-02-13 18:40:05 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	comlib.js
    	cs4.js
    	package.json
    	public/Scripts/cs4Edit.js
    	public/Scripts/cs4Home.js
    	public/Scripts/cs4Settings.js
    	public/Scripts/cs4Timing.js
    	public/Scripts/cs4VerticalCanvas.js
    	public/Scripts/data.js
    	public/stylesheets/cs4Home.css
    	public/stylesheets/cs4Info.css
    	public/stylesheets/cs4Menu.css
    	public/stylesheets/cs4Settings.css
    	public/stylesheets/cs4Timing.css
    	public/stylesheets/cs4VerticalCanvas.css
    	routes/index.js
    	views/CS4VerticalScroll.ejs
    	views/cs4Help.ejs
    	views/cs4Home.ejs
    	views/cs4Info.ejs
    	views/cs4Settings.ejs
    	views/cs4Timing.ejs

--- public/Scripts/cs4Edit.js
+++ public/Scripts/cs4Edit.js
@@ -58,7 +58,7 @@ function onMessage(evt)    {
     message = JSON.parse(evt.data);
     if(message.packetType == 'cuefiledata'){
         inMessage = message.data;
-        writeToScreen(inMessage);
+        pixelLoad(inMessage);
     }
     else{
        writeToScreen(inMessage);
@@ -89,5 +89,47 @@ function writeToScreen(message) {
 function loadclick(){
     websocket.send('EDIT');//
 }
+
+function pixelLoad(item){
+    //
+    //  (new Date( lastconverted.setMilliseconds(lastconverted.getMilliseconds() + (timerStartTime -lastCueReceivedInternalTime)))).toISOString()
+    //
+    var i =0;
+    var j = 0;
+    var count = 0;
+    var dte;
+    if (item.length == 0) {
+        console.log("not Found");
+    }
+    else {
+        var packet = {};
+        //  packet.Time = new Date(new Date(item[i].Time) + item[i].OutData[j].Delay).toISOString();
+        packet.Time = new Date(item[i].Time).toISOString();
+        packet.Time = item[i].OutData[j].Delay;
+        packet.Time =  new Date(item[i].Time).toISOString() + item[i].OutData[j].Delay;
+        for(var i = 0; i< item[0].OutData.length; i++)
+        {
+            dir = item[0].OutData[i].Dir;    // ****** needs to ba added to R4-4 Receiver Parsing ****** //
+            // dir = "xxxx";
+            port = item[0].OutData[i].Port.toUpperCase();
+            showname = item[0].OutData[i].Showname;
+            dataToSend = item[0].OutData[i].Dout;
+            delay = item[0].OutData[i].Delay;
+            if(dir =="")
+            {
+                outstring = port + " " + showname + " " + dataToSend;
+            }
+            else
+            {
+                outstring = port + " " + showname + " " + dir + " " + dataToSend;
+            }
+
+            setTimeout(sendOutput, delay, outstring);
+            console.log(item[0].OutData[i].Dout + "  Delay "+item[0].OutData[i].Delay);
+        }
+
+    }
+}
+
 
 
