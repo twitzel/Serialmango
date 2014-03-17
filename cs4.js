cef9c60223b1a608151f9440279217acb9a30f9c
stevewitz <switzel@wizcomputing.com>
2014-03-17 14:08:15 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	cs4.js

--- cs4.js
+++ cs4.js
@@ -506,11 +506,10 @@ if(data.length >= 35) // this is to let GETTIME come through and get logged GETT
     lastCueReceived = (JSON.parse(JSON.stringify(serialData))); // store the data here in case of Cue file generation
     //get the internal system time or this event so we and keep track of it
 
-
-
-    lastCueReceivedInternalTime = new Date();
-    lastCueReceivedExternalTime = new Date(lastCueReceived);
-
+    if(serialData.Source != 'zigbee2:'){ //only update if real cue NOT zigbee2
+        lastCueReceivedInternalTime = new Date();
+        lastCueReceivedExternalTime = new Date(lastCueReceived);
+    }
     //Log the data into the collection
 
     collectionLog.insert(serialData, {w: 1}, function (err, result) {
