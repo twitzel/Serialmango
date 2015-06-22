829ec7960400cdc7cac6efd8514a2799aa6f0fd8
stevewitz <switzel@wizcomputing.com>
2015-06-22 12:43:45 -0700

    mdi filter delay in sending

--- cs4.js
+++ cs4.js
@@ -436,7 +436,7 @@ exports.websocketDataIn = function(dataSocket, Socket){
             dataToSend = '          SLAVE DMX_CH ' + cs4Settings.dmx1 +  " " + cs4Settings.dmx2 + " " + cs4Settings.dmx3 + ''; //update the DMX channels
             sendOutput(dataToSend) ;
             timeoutlist = [];
-            comlib.websocketsend("Successfully updated settings file");
+            comlib.websocketsend("Successfully updated new settings file");
            dataToSend = '          SLAVE ZIGEN ' + cs4Settings.enableZigbee2 ; //update the DMX channels
             timeoutlist[1] = setTimeout(sendOutput, 1000, dataToSend); //setTimeout(function(){sendOutput(dataToSend);}, 3000);
             dataToSend = '          MIDIFIL1 ' + cs4Settings.midisex1 + " " + cs4Settings.nonsysex1 + " "+ (parseInt(+cs4Settings.deviceIDLow1)*1 + parseInt(+cs4Settings.deviceIDHigh1)*16).toString() + " " + cs4Settings.type1 + " " + cs4Settings.commandformat1 + " " +   cs4Settings.command1 + " " + cs4Settings.nonsysextype1 + " " + cs4Settings.nonsysexchannel1 + " ";
