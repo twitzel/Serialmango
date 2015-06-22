ca4f3b8de6a7badc9283ea82b6b912ee0f71c14c
stevewitz <switzel@wizcomputing.com>
2015-06-22 12:14:23 -0700

    mdi filter delay in sending

--- cs4.js
+++ cs4.js
@@ -441,7 +441,7 @@ exports.websocketDataIn = function(dataSocket, Socket){
           //  setTimeout(sendOutput, 1000, dataToSend); //setTimeout(function(){sendOutput(dataToSend);}, 3000);
             comlib.websocketsend("Sent SALVE ZIGEN");
             dataToSend = '          MIDIFIL1 ' + cs4Settings.midisex1 + " " + cs4Settings.nonsysex1 + " "+ (parseInt(+cs4Settings.deviceIDLow1)*1 + parseInt(+cs4Settings.deviceIDHigh1)*16).toString() + " " + cs4Settings.type1 + " " + cs4Settings.commandformat1 + " " +   cs4Settings.command1 + " " + cs4Settings.nonsysextype1 + " " + cs4Settings.nonsysexchannel1 + " ";
-            setTimeout(sendOutput, 2000, dataToSend); //setTimeout(function(){sendOutput(dataToSend);}, 6000);  // sendOutput(dataToSend);
+            var a = setTimeout(sendOutput, 2000, dataToSend); //setTimeout(function(){sendOutput(dataToSend);}, 6000);  // sendOutput(dataToSend);
             comlib.websocketsend("Sent MIDIFIL1");
             dataToSend = '          MIDIFIL2 ' + cs4Settings.midisex2 + " " + cs4Settings.nonsysex2 + " "+ (parseInt(+cs4Settings.deviceIDLow2)*1 + parseInt(+cs4Settings.deviceIDHigh2)*16).toString() + " " + cs4Settings.type2 + " " + cs4Settings.commandformat2 + " " +   cs4Settings.command2 + " " + cs4Settings.nonsysextype2 + " " + cs4Settings.nonsysexchannel2 + " ";
             setTimeout(sendOutput, 3000, dataToSend); //setTimeout(function(){sendOutput(dataToSend);}, 9000);  //sendOutput(dataToSend);
