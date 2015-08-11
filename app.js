c622f896877c18f33e376d9f5a10b4d0915b9ecd
stevewitz <switzel@wizcomputing.com>
2015-08-11 09:05:46 -0700

    app.get moved

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ function startwebserver(){
   http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 
-      startwebsocketserver();
+      comlib.startwebsocketserver();
 
   });
   
