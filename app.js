ca9e6bca3b6d5460c764adf5c8f4a3e395c46387
stevewitz <switzel@wizcomputing.coom>
2016-03-16 16:18:56 -0700

    app.js changes

--- app.js
+++ app.js
@@ -66,12 +66,7 @@ app.get('/cs4Edit', routes.cs4Edit);
 
 
 // Set up the server
-function startwebserver(){
-  http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-  });
-  
-}
+
 
 /*
     Set up for the desired branch
@@ -93,9 +88,15 @@ else if(branch == 'cs4')
 {
     var cs4 = require('./cs4');
     cs4.ledOff(); // turn off the ready led
-    cs4.setup(startwebserver());
+    cs4.setup();
+    startwebserver();
 }
 
+function startwebserver(){
+    http.createServer(app).listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
 
+}
 
 
