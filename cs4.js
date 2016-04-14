de159a907545ae9d9aaf62c7279e7a6e4fa593d7
stevewitz <switzel@wizcomputing.coom>
2016-04-14 15:48:55 -0700

    rpio setmode line 1451

--- cs4.js
+++ cs4.js
@@ -1448,7 +1448,7 @@ exports.ledOff = function(){
          */ clearInterval(blink);
 
         var rpio = require('rpi-gpio');///var rpio = require('rpio');
-        rpio.setmode('gpio');
+        rpio.setMode('gpio');
         rpio.setOutput(4);
         rpio.write(4, rpio.LOW);
         clearInterval(blink);
